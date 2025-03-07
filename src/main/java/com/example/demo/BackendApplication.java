f5dc1fbe3687213a5e6deedf4afaea4c6d95bfbe
ian otieno <ianotieno23@gmail.com>
2025-03-07 09:39:27 +0300

    update

--- src/main/java/com/example/demo/BackendApplication.java
+++ src/main/java/com/example/demo/BackendApplication.java
@@ -1,11 +1,15 @@
 package com.example.demo;
 
+import java.time.LocalDate;
+import java.time.Month;
 import java.util.List;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.student.Student;
 
 @SpringBootApplication
 @RestController
@@ -16,7 +20,13 @@ public class BackendApplication {
 	}
 
 	@GetMapping
-	public List <String> hello(){
-		return List.of("Hello", "World");
+	public List <Student> hello(){
+		return List.of(
+				new Student(
+					1L, "Mariam",
+				 "mariam.student.com", 
+				 21, 
+				 LocalDate.of(2000, Month.JANUARY, 5))
+				);
 	}
 }
