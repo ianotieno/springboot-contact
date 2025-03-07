881cca99e2a6cbd24152b6abb8abbd47590aa59c
ian otieno <ianotieno23@gmail.com>
2025-03-07 10:55:51 +0300

    update controller

--- src/main/java/com/example/demo/student/StudentController.java
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -1,22 +1,22 @@
 package com.example.demo.student;
-
-import java.time.LocalDate;
-import java.time.Month;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class StudentController {
-@GetMapping
-	public List <Student> hello(){
-		return List.of(
-				new Student(
-					1L, "Mariam",
-				 "mariam.student.com", 
-				 21, 
-				 LocalDate.of(2000, Month.JANUARY, 5))
-				);
+    @Autowired
+    private final StudentService studentService;
+
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
+
+@GetMapping(path = "api/v1/student")
+	public List <Student> getStudents(){
+		return studentService.getStudents();
 	}
 }
